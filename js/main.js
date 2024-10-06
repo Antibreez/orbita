797e577dd6e9cbd16e0226a58ccb5e0bd825d65b
Anton Kiyan <kiyananton@gmail.com>
2024-10-06 23:27:53 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+};
+
+const headBlock = document.querySelector('.head-block');
+
+docReady(function () {
+  console.log('###ready');
+  headBlock.classList.add('ready');
+});
+
+//# sourceMappingURL=main.js.map
